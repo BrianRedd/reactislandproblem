71771abef2f54b697cf682952c51cbb6549f02ca
Brian Redd <bredd@trinidadbenham.com>
2020-03-08 16:48:22 -0600

    completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Grid from "./Grid";
+import Calculate from "./Calculate";
 
 function App() {
+  const length = 5;
+
+  const [grid, setGrid] = useState(null);
+  const [visited, setVisited] = useState(null);
+
+  const createGrid = () => {
+    const temp = [];
+    const vTemp = [];
+    for (let i = 0; i < length; i += 1) {
+      const row = [];
+      const vRow = [];
+      for (let ii = 0; ii < length; ii += 1) {
+        row.push(Math.round(Math.random()));
+        vRow.push(0);
+      }
+      temp.push(row);
+      vTemp.push(vRow);
+    }
+    setGrid(temp);
+    setVisited(vTemp);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Island Problem</h1>
+      {grid && (
+        <React.Fragment>
+          <Grid grid={grid} />
+          <Calculate grid={grid} visited={visited} createGrid={createGrid} />
+        </React.Fragment>
+      )}
+      {!grid && <button onClick={createGrid}>New Grid</button>}
     </div>
   );
 }
